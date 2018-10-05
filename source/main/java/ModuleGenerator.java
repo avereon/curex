93a9508edf55cbe7333a678c5afbfecaa134dc69
Mark Soderquist <mark@soderquist.net>
2018-10-05 15:05:20 -0600

    Fix problems patching modules with dependencies

--- source/main/java/ModuleGenerator.java
+++ source/main/java/ModuleGenerator.java
@@ -1,8 +1,10 @@
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.module.ModuleFinder;
 import java.lang.module.ModuleReference;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -38,28 +40,28 @@ public class ModuleGenerator {
 	}
 
 	public void execute( String[] commands ) {
-		List<File> files = Arrays.stream( commands ).map( File::new ).collect( Collectors.toList() );
+		List<File> files = Arrays.stream( commands ).map( ( name ) -> new File( getModulePath(), name ) ).collect( Collectors.toList() );
 
 		for( File file : files ) {
-			if( file.isDirectory() ) {
-				for( File child : Objects.requireNonNull( file.listFiles() ) ) {
-					process( child );
+			try {
+				int result = patch( file );
+				switch( result ) {
+					case 0: {
+						System.out.println( "Patch module: " + file.getName() );
+						break;
+					}
+					case 1: {
+						System.out.println( "Ready module: " + file.getName() );
+						break;
+					}
 				}
-			} else {
-				process( file );
+			} catch( Exception exception ) {
+				exception.printStackTrace( System.err );
 			}
 		}
 	}
 
-	private void process( File file ) {
-		try {
-			init( new File( getModulePath(), file.getName() ) );
-		} catch( Exception exception ) {
-			exception.printStackTrace( System.err );
-		}
-	}
-
-	private void init( File file ) throws Exception {
+	public int patch( File file ) throws Exception {
 		if( !file.exists() ) throw new FileNotFoundException( file.toString() );
 
 		Set<ModuleReference> moduleReferences = ModuleFinder.of( file.toPath() ).findAll();
@@ -75,32 +77,46 @@ public class ModuleGenerator {
 			ModuleReference reference = moduleReferences.iterator().next();
 			if( reference.descriptor().isAutomatic() ) {
 				moduleName = reference.descriptor().name();
-				//System.out.println( "Automatic module name: " + moduleName );
 			} else {
-				System.out.println( "Already module: " + reference.descriptor().name() );
-				return;
+				return 1;
 			}
 		}
 
 		if( moduleName == null ) moduleName = file.getName().replace( "-", "." );
 
-		fix( file, moduleName );
+		patch( file, moduleName );
+
+		if( isAutomaticModule( file ) ) throw new RuntimeException( "Module is still an automatic module: " + moduleName );
+
+		return 0;
 	}
 
-	private void fix( File file, String moduleName ) throws IOException, InterruptedException {
-		System.out.println( "Produce module: " + moduleName );
+	private boolean isAutomaticModule( File file ) {
+		Set<ModuleReference> moduleReferences = ModuleFinder.of( file.toPath() ).findAll();
+
+		if( moduleReferences.size() > 1 ) {
+			List<String> names = moduleReferences.stream().map( ( reference ) -> reference.descriptor().name() ).collect( Collectors.toList() );
+			throw new IllegalArgumentException( "Jar contains more than one module: " + names.toString() );
+		}
+
+		return moduleReferences.iterator().next().descriptor().isAutomatic();
+	}
+
+	private void patch( File file, String moduleName ) throws IOException, InterruptedException {
+		System.out.println( "Patch module: " + moduleName );
 
 		File workFolder = new File( getWorkFolder() );
 		File tempModule = new File( workFolder, file.getName() );
 
-		workFolder.mkdirs();
-
 		// Move the module to fix out of the module path
-		file.renameTo( tempModule );
-		patchModule( moduleName, workFolder, tempModule );
-		tempModule.renameTo( file );
-
-		deleteAll( workFolder );
+		try {
+			workFolder.mkdirs();
+			file.renameTo( tempModule );
+			patchModule( moduleName, workFolder, tempModule );
+		} finally {
+			tempModule.renameTo( file );
+			deleteAll( workFolder );
+		}
 	}
 
 	private void patchModule( String moduleName, File workFolder, File tempModule ) throws IOException, InterruptedException {
@@ -112,15 +128,41 @@ public class ModuleGenerator {
 		File moduleInfoFolder = new File( workFolder, moduleName );
 		File moduleInfo = new File( moduleInfoFolder, "module-info.java" );
 
-		exec( jdeps.toString(), "--module-path", modulePath, "--generate-module-info", workFolder.toString(), tempModule.toString() );
-		exec( javac.toString(), "-p", modulePath, "--patch-module", moduleName + "=" + tempModule, moduleInfo.toString() );
-		exec( jar.toString(), "uf", tempModule.toString(), "-C", moduleInfoFolder.toString(), "module-info.class" );
+		String jdepsResult = exec( false, jdeps.toString(), "--module-path", modulePath, "--add-modules=ALL-MODULE-PATH", "--generate-module-info", workFolder.toString(), tempModule.toString() );
+		if( !"".equals( jdepsResult ) ) {
+			System.err.println( jdepsResult );
+			return;
+		}
+		String javacResult = exec( false, javac.toString(), "-p", modulePath, "--patch-module", moduleName + "=" + tempModule, moduleInfo.toString() );
+		if( !"".equals( javacResult ) ) {
+			System.err.println( javacResult );
+			return;
+		}
+		String jarResult = exec( false, jar.toString(), "uf", tempModule.toString(), "-C", moduleInfoFolder.toString(), "module-info.class" );
+		if( !"".equals( jarResult ) ) {
+			System.err.println( jarResult );
+			return;
+		}
 	}
 
-	private void exec( String... commands ) throws IOException, InterruptedException {
+	private String exec( boolean log, String... commands ) throws IOException, InterruptedException {
+		ByteArrayOutputStream output = new ByteArrayOutputStream();
+
 		ProcessBuilder builder = new ProcessBuilder( commands );
+		if( log ) {
+			System.out.println( builder.command() );
+			builder.inheritIO();
+		} else {
+			builder.redirectErrorStream( true );
+		}
 		Process process = builder.start();
 		process.waitFor( 10, TimeUnit.SECONDS );
+
+		process.getInputStream().transferTo( output );
+
+		int result = process.exitValue();
+
+		return result == 0 ? "" : output.toString( StandardCharsets.UTF_8.toString() );
 	}
 
 	private static void deleteAll( File file ) {
--- source/main/java/PatchMojo.java
+++ source/main/java/PatchMojo.java
@@ -5,19 +5,24 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Generally used after the maven-dependency-plugin to patch jars that are not
  * yet modules.
  */
 
-@Mojo( name = "patch",  defaultPhase = LifecyclePhase.PREPARE_PACKAGE )
+@Mojo( name = "patch", defaultPhase = LifecyclePhase.PREPARE_PACKAGE )
 public class PatchMojo extends AbstractMojo {
 
-	@Parameter( property = "modulePath", defaultValue = "target/dependency" )
+	@Parameter( property = "modulePath", defaultValue = "${project.build.directory}/dependency" )
 	private String modulePath;
 
 	@Parameter( property = "modules" )
-	private String[] modules;
+	private String[] jars;
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
@@ -25,7 +30,29 @@ public class PatchMojo extends AbstractMojo {
 
 		getLog().info( "Module path: " + generator.getModulePath() );
 
-		generator.execute( modules );
+		List<File> files = Arrays.stream( jars ).map( ( name ) -> new File( getModulePath(), name ) ).collect( Collectors.toList() );
+
+		for( File file : files ) {
+			try {
+				if( file.isFile() ) {
+					int result = generator.patch( file );
+					switch( result ) {
+						case 0: {
+							getLog().info( "Patch module: " + file.getName() );
+							break;
+						}
+						case 1: {
+							getLog().info( "Ready module: " + file.getName() );
+							break;
+						}
+					}
+				}
+			} catch( Throwable throwable ) {
+				getLog().error( "Error occurred generating module patch", throwable );
+			}
+
+		}
+
 	}
 
 	public String getModulePath() {
@@ -36,12 +63,12 @@ public class PatchMojo extends AbstractMojo {
 		this.modulePath = modulePath;
 	}
 
-	public String[] getModules() {
-		return modules;
+	public String[] getJars() {
+		return jars;
 	}
 
-	public void setModules( String[] modules ) {
-		this.modules = modules;
+	public void setJars( String[] jars ) {
+		this.jars = jars;
 	}
 
 }
