5aca50a205d3c5d0b50c8de70be70cb1afd8f2a2
Mark Soderquist <mark@soderquist.net>
2018-10-05 12:56:46 -0600

    Set default maven phase

--- source/main/java/PatchMojo.java
+++ source/main/java/PatchMojo.java
@@ -1,12 +1,18 @@
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.util.List;
 
-@Mojo( name = "patch" )
+/**
+ * Generally used after the maven-dependency-plugin to patch jars that are not
+ * yet modules.
+ */
+
+@Mojo( name = "patch",  defaultPhase = LifecyclePhase.PREPARE_PACKAGE )
 public class PatchMojo extends AbstractMojo {
 
 	@Parameter( property = "modulePath", defaultValue = "target/dependency" )
